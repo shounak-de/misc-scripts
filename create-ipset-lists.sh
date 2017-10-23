20c832290b7c58be713ea86e816ac969e2f16eaf
redhat27 <shounak.de@gmail.com>
2017-10-22 19:25:04 -0700

    Corrected reload logic if script is run in cron

--- create-ipset-lists.sh
+++ create-ipset-lists.sh
@@ -1,105 +1,3 @@
-#!/bin/sh
-# snbforums thread:
-# https://www.snbforums.com/threads/country-blocking-script.36732/page-2#post-311407
-
-# Re-download blocklist if locally saved blocklist is older than this many days
-BLOCKLISTS_SAVE_DAYS=15
-
-# For the users of mips routers (kernel 2.x): You can now block sources with IPv6 with country blocklists
-# Enable if you want to add huge country IPv6 netmask lists directly into ip6tables rules.
-# Also, enabling this will add a *lot* of processing time!
-# Note: This has no effect *if* you have ipset v6: It will always use ipset v6 for IPv6 country blocklists regardless of whether this is enabled or not.
-USE_IP6TABLES_IF_IPSETV6_UNAVAILABLE=disabled # [enabled|disabled]
-
-# Block incoming traffic from some countries. cn and pk is for China and Pakistan. See other countries code at http://www.ipdeny.com/ipblocks/
-BLOCKED_COUNTRY_LIST="ar au br cn de fr jp kp kr pk ru sa sc tr tw ua vn"
-
-# Use DROP or REJECT for iptable rule for the ipset. Briefly, for DROP, attacker (or IP being blocked) will get no response and timeout, and REJECT will send immediate response of destination-unreachable (Attacker will know your IP is actively rejecting requests)
-# See: http://www.chiark.greenend.org.uk/~peterb/network/drop-vs-reject and http://serverfault.com/questions/157375/reject-vs-drop-when-using-iptables
-IPTABLES_RULE_TARGET=DROP # [DROP|REJECT]
-
-# Preparing folder to cache downloaded files
-IPSET_LISTS_DIR=/jffs/ipset_lists
-[ -d "$IPSET_LISTS_DIR" ] || mkdir -p $IPSET_LISTS_DIR
-
-# Different routers got different iptables and ipset syntax
-case $(ipset -v | grep -o "v[4,6]") in
-  v6)
-    MATCH_SET='--match-set'; CREATE='create'; ADD='add'; SWAP='swap'; IPHASH='hash:ip'; NETHASH='hash:net family inet'; NETHASH6='hash:net family inet6'; SETNOTFOUND='name does not exist'
-    # Loading ipset modules
-    lsmod | grep -q "xt_set" || \
-    for module in ip_set ip_set_hash_net ip_set_hash_ip xt_set; do
-      modprobe $module
-    done;;
-  v4)
-    MATCH_SET='--set'; CREATE='--create'; ADD='--add'; SWAP='--swap'; IPHASH='iphash'; NETHASH='nethash'; SETNOTFOUND='Unknown set'
-    # Loading ipset modules
-    lsmod | grep -q "ipt_set" || \
-    for module in ip_set ip_set_nethash ip_set_iphash ipt_set; do
-      modprobe $module
-    done;;
-  *)
-    logger -t Firewall "$0: Unknown ipset version: $(ipset -v). Exiting."
-    exit 1;;
-esac
-
-# Wait if this is run early on (before the router has internet connectivity) [Needed by wget to download files]
-while ! ping -q -c 1 google.com &>/dev/null; do
-  sleep 1
-  WaitSeconds=$((WaitSeconds+1))
-  [ $WaitSeconds -gt 300 ] && logger -t Firewall "$0: Warning: Router not online! Aborting after a wait of 5 minutes..." && exit 1
-done
-
-# Block traffic from Tor nodes [IPv4 nodes only]
-if $(ipset $SWAP TorNodes TorNodes 2>&1 | grep -q "$SETNOTFOUND"); then
-  ipset $CREATE TorNodes $IPHASH
-  [ $? -eq 0 ] && entryCount=0
-  [ ! -e "$IPSET_LISTS_DIR/tor.lst" -o -n "$(find $IPSET_LISTS_DIR/tor.lst -mtime +$BLOCKLISTS_SAVE_DAYS -print 2>/dev/null)" ] && wget -q -O $IPSET_LISTS_DIR/tor.lst http://torstatus.blutmagie.de/ip_list_all.php/Tor_ip_list_ALL.csv
-  for IP in $(cat $IPSET_LISTS_DIR/tor.lst); do
-    ipset $ADD TorNodes $IP
-    [ $? -eq 0 ] && entryCount=$((entryCount+1))
-  done
-  logger -t Firewall "$0: Added TorNodes list ($entryCount entries)"
-fi
-iptables-save | grep -q TorNodes || iptables -I INPUT -m set $MATCH_SET TorNodes src -j $IPTABLES_RULE_TARGET
-
-# Country blocking by nethashes [Both IPv4 and IPv6 sources]
-if $(ipset $SWAP BlockedCountries BlockedCountries 2>&1 | grep -q "$SETNOTFOUND"); then
-  ipset $CREATE BlockedCountries $NETHASH
-  for country in ${BLOCKED_COUNTRY_LIST}; do
-    entryCount=0
-    [ ! -e "$IPSET_LISTS_DIR/$country.lst" -o -n "$(find $IPSET_LISTS_DIR/$country.lst -mtime +$BLOCKLISTS_SAVE_DAYS -print 2>/dev/null)" ] && wget -q -O $IPSET_LISTS_DIR/$country.lst http://www.ipdeny.com/ipblocks/data/aggregated/$country-aggregated.zone
-    for IP in $(cat $IPSET_LISTS_DIR/$country.lst); do
-      ipset $ADD BlockedCountries $IP
-      [ $? -eq 0 ] && entryCount=$((entryCount+1))
-    done
-    logger -t Firewall "$0: Added country [$country] to BlockedCountries list ($entryCount entries)"
-  done
-fi
-iptables-save | grep -q BlockedCountries || iptables -I INPUT -m set $MATCH_SET BlockedCountries src -j $IPTABLES_RULE_TARGET
-if [ $(nvram get ipv6_fw_enable) -eq 1 -a "$(nvram get ipv6_service)" != "disabled" ]; then
-  if $(ipset $SWAP BlockedCountries6 BlockedCountries6 2>&1 | grep -q "$SETNOTFOUND"); then
-    [  -n "$NETHASH6" ] && ipset $CREATE BlockedCountries6 $NETHASH6
-    for country in ${BLOCKED_COUNTRY_LIST}; do
-      [ -e "/tmp/ipv6_country_blocks_loaded" ] && logger -t Firewall "$0: Country block rules has already been loaded into ip6tables... Skipping." && break
-      entryCount=0
-      [  -n "$NETHASH6" -o $USE_IP6TABLES_IF_IPSETV6_UNAVAILABLE = "enabled" ] && [ ! -e "$IPSET_LISTS_DIR/${country}6.lst" -o -n "$(find $IPSET_LISTS_DIR/${country}6.lst -mtime +$BLOCKLISTS_SAVE_DAYS -print 2>/dev/null)" ] && wget -q -O $IPSET_LISTS_DIR/${country}6.lst http://www.ipdeny.com/ipv6/ipaddresses/aggregated/${country}-aggregated.zone
-      for IP6 in $(cat $IPSET_LISTS_DIR/${country}6.lst); do
-        if [ -n "$NETHASH6" ]; then
-          ipset $ADD BlockedCountries6 $IP6
-        elif [ $USE_IP6TABLES_IF_IPSETV6_UNAVAILABLE = "enabled" ]; then
-          ip6tables -I INPUT -s $IP6 -j $IPTABLES_RULE_TARGET
-        fi
-        [ $? -eq 0 ] && entryCount=$((entryCount+1))
-      done
-      if [ -n "$NETHASH6" ]; then
-        logger -t Firewall "$0: Added country [$country] to BlockedCountries6 list ($entryCount entries)"
-      elif [ $USE_IP6TABLES_IF_IPSETV6_UNAVAILABLE = "enabled" ]; then
-        logger -t Firewall "$0: Added country [$country] to ip6tables rules ($entryCount entries)"
-      fi
-    done
-  fi
-  if [ -n "$NETHASH6" ]; then
     ip6tables -L | grep -q BlockedCountries6 || ip6tables -I INPUT -m set $MATCH_SET BlockedCountries6 src -j $IPTABLES_RULE_TARGET
   elif [ $USE_IP6TABLES_IF_IPSETV6_UNAVAILABLE = "enabled" -a ! -e "/tmp/ipv6_country_blocks_loaded" ]; then
     logger -t Firewall "$0: Creating [/tmp/ipv6_country_blocks_loaded] to prevent accidental reloading of country blocklists in ip6table rules."
@@ -139,16 +37,17 @@ if [ -e $IPSET_LISTS_DIR/custom.lst ]; then
   iptables-save | grep -q CustomBlock || iptables -I INPUT -m set $MATCH_SET CustomBlock src -j $IPTABLES_RULE_TARGET
 fi
 
-# Allow traffic from AcceptList [IPv4 only] [$IPSET_LISTS_DIR/whitelist.lst can contain a combination of IPv4 IP or IPv4 netmask]
+# Allow traffic from AllowList [IPv4 only] [$IPSET_LISTS_DIR/whitelist.lst can contain IPv4 IPs]
 if [ -e $IPSET_LISTS_DIR/whitelist.lst ]; then
-  if $(ipset $SWAP AcceptList AcceptList 2>&1 | grep -q "$SETNOTFOUND"); then
-    ipset $CREATE AcceptList $NETHASH
+  if $(ipset $SWAP AllowList AllowList 2>&1 | grep -q "$SETNOTFOUND"); then
+    ipset $CREATE AllowList $IPHASH #(was $NETHASH)
     [ $? -eq 0 ] && entryCount=0
     for IP in $(cat $IPSET_LISTS_DIR/whitelist.lst); do
-      [ "${IP##*/}" == "$IP" ] && ipset $ADD AcceptList $IP/31 || ipset $ADD AcceptList $IP
+      #[ "${IP##*/}" == "$IP" ] && ipset $ADD AllowList $IP/31 || ipset $ADD AllowList $IP
+      ipset $ADD AllowList $IP
       [ $? -eq 0 ] && entryCount=$((entryCount+1))
     done
-    logger -t Firewall "$0: Added AcceptList ($entryCount entries)"
+    logger -t Firewall "$0: Added AllowList ($entryCount entries)"
   fi
-  iptables-save | grep -q AcceptList || iptables -I INPUT -m set $MATCH_SET AcceptList src -j ACCEPT
+  iptables-save | grep -q AllowList || iptables -I INPUT -m set $MATCH_SET AllowList src -j ACCEPT
 fi
